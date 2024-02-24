e37190ea9950af5ba2d4edcdd8dd1b70dc80d632
kiphon <159582368+kiphon@users.noreply.github.com>
2024-02-24 13:38:46 +0200

    Add files via upload

---
+++ weeklycode.py
@@ -0,0 +1,7 @@
+# ask the user for their name
+name=input("What is your name? ")
+# ask the user for their Age
+age=input("How old are you? ")
+# ask the user for their location
+location=input("Where do you live? ")
+print("Hello {}, you are {} years old and lives in {}.".format(name,age,location))
